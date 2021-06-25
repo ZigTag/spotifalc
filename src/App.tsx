640819de625d5e11b113798af0d3bb3082085d00
ZigTag <17077422+ZigTag@users.noreply.github.com>
2021-06-24 22:40:13 -0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, { PropsWithChildren } from 'react';
+import { IconX, IconRectangle, IconMinus } from '@tabler/icons';
+import { appWindow } from '@tauri-apps/api/window';
+
+type ButtonProps = { id?: string, className?: string, onClick?: () => void, isClose?: boolean }
+
+const Button: React.FC<ButtonProps> = ({ id, className, onClick, isClose, children }: PropsWithChildren<ButtonProps>) => (
+    <button
+        type="button"
+        className={
+            `w-12 h-full flex flex-row justify-center items-center text-[#929190] ${isClose ? 'hover:bg-red-500 hover:bg-opacity-50' : 'hover:bg-black hover:bg-opacity-10'} ${className}`
+        }
+        onClick={onClick ?? (() => {})}
+        id={id}
+    >
+        {children}
+    </button>
+);
+
+const TopBar: React.FC = () => (
+    <div className="h-8 w-full bg-[#0B0B0B] bg-opacity-25 select-none drag-region">
+        <div className="h-full w-min ml-auto flex flex-row">
+            <Button onClick={appWindow.minimize}><IconMinus size="15" /></Button>
+            <Button onClick={appWindow.maximize}><IconRectangle size="15" /></Button>
+            <Button onClick={appWindow.close} isClose><IconX size="15" /></Button>
+        </div>
+    </div>
+);
+
+const App: React.FC = () => (
+    <div>
+        <TopBar />
+        <p>Testing Text</p>
+    </div>
+);
+
+export default App;
